13b3844e738a97f2c6367729925491944e916ce0
nuno-21902803 <nunomaluko5@gmail.com>
2021-05-21 12:31:48 +0100

    pray v3

--- djangoProject_lab_pw_6/settings.py
+++ djangoProject_lab_pw_6/settings.py
@@ -116,7 +116,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'website/static'),
 )
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
